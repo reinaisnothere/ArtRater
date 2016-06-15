f8aa675c21388b22fb3d28084f11c88c9fcc5c81
Reina Mei <reinameic@gmail.com>
2016-06-15 16:43:07 -0700

    Change voting button color on hover

--- client/app.js
+++ client/app.js
@@ -6,6 +6,7 @@ appModule.controller('AppController', function($http, $scope) {
   var count;
   $scope.more = true;
   $scope.end = false;
+
   $http({
     method: 'GET',
     url: '/submissions?count=true',
@@ -53,39 +54,4 @@ appModule.controller('AppController', function($http, $scope) {
       $scope.end = true;
     });
   };
-  
-  // $scope.places = [];
-  // $scope.loading = false;
-  // $scope.showError = false;
-  // $scope.search = function() {
-  //   $scope.loading = true;
-  //   $scope.showError = false;
-  //   $scope.places = [];
-  //   navigator.geolocation.getCurrentPosition(function(position) {
-  //     var latitude = position.coords.latitude;
-  //     var longitude = position.coords.longitude;
-  //     var location = latitude + ',' + longitude;
-  //     $http({
-  //       method: 'GET',
-  //       url: '/places?' + 'keyword=' + $scope.keyword + '&location=' + location
-  //     })
-  //     .then(function(results) {
-  //       $scope.loading = false;
-  //       if (!results.data.results.length) {
-  //         $scope.showError = true;
-  //       } else {
-  //         $scope.places = results.data.results.map(function(place) {
-  //           return {
-  //             vicinity: place.vicinity,
-  //             name: place.name,
-  //             url: place.name + ' ' + place.vicinity
-  //           }
-  //         });
-  //       }
-  //     }, function(err) {
-  //       $scope.loading = false;
-  //       $scope.showError = true;
-  //     });
-  //   });
-  // }
 });
