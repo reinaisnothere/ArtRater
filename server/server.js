c32f4d2aca7b75d0ee3cc4dc666bd57ec8166d8d
Reina Mei <reinameic@gmail.com>
2016-06-15 13:52:18 -0700

    Handle GET to /submissions

--- server/server.js
+++ server/server.js
@@ -5,6 +5,21 @@ var db = require('./db.js');
 
 app.use(express.static(__dirname + '/../client'));
 
+app.get('/submissions', function(req, res) {
+  if (req.query.count) {
+    return res.json(db.length);
+  }
+  res.json(db);
+});
+
+app.get('/submissions/:id', function(req, res) {
+  var id = JSON.parse(req.params.id);
+  if (id < db.length && id >= 0) {
+    return res.json(db[id]);
+  }
+  res.sendStatus(404);
+});
+
 app.listen(3000, function() {
   console.log('artRater listening on port 3000...');
 });
