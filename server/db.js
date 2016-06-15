797addf04eb69ff0d7a801c7642069efed6fef6d
Reina Mei <reinameic@gmail.com>
2016-06-15 13:23:15 -0700

    Add express to dependencies; add gitignore file

---
+++ server/db.js
@@ -0,0 +1,40 @@
+module.exports = [{
+    "design_name": "Gesturing Man",
+    "design_thumbnail": "http://mintedcdn0.global.ssl.fastly.net/files/mintedProductsImages/MIN/GNA/3U2/MIN-3U2-GNA-007B_A_PZ.jpg?_=13802",
+    "artist_name": "Derek Overfield",
+    "artist_location": "Fairmont, WV",
+    "artist_thumbnail": "http://cdn3.minted.com/files/content/designers/100_714409_5636336a55220.jpg"
+  },
+
+  {
+    "design_name": "Prague",
+    "design_thumbnail": "http://mintedcdn0.global.ssl.fastly.net/files/mintedProductsImages/MIN/GNA/NYD/MIN-NYD-GNA-007B_A_PZ.jpg?_=13802",
+    "artist_name": "Anastasia Makarova",
+    "artist_location": "Moscow, RU",
+    "artist_thumbnail": "http://cdn3.minted.com/files/content/designers/200_1373181_8e50b9c62f55e.jpg"
+  },
+
+  {
+    "design_name": "Read Between the Lines",
+    "design_thumbnail": "http://mintedcdn0.global.ssl.fastly.net/files/mintedProductsImages/MIN/GNA/QU4/MIN-QU4-GNA-007B_A_PZ.jpg?_=13802",
+    "artist_name": "Cecelia Hospers",
+    "artist_location": "San Francisco, CA",
+    "artist_thumbnail": "http://cdn3.minted.com/files/content/designers/200_913637_0ac687ae1eed8.jpg"
+  },
+
+  {
+    "design_name": "Embrace",
+    "design_thumbnail": "http://mintedcdn0.global.ssl.fastly.net/files/mintedProductsImages/MIN/GNA/HFD/MIN-HFD-GNA-007B_A_PZ.jpg?_=13802",
+    "artist_name": "R Studio",
+    "artist_location": "Syracuse, NY",
+    "artist_thumbnail": "http://cdn3.minted.com/files/content/designers/100_10023_547b71ff59901.jpg"
+  },
+
+  {
+    "design_name": "Natural",
+    "design_thumbnail": "http://mintedcdn0.global.ssl.fastly.net/files/mintedProductsImages/MIN/GNA/XKM/MIN-XKM-GNA-007B_A_PZ.jpg?_=13802",
+    "artist_name": "Makewells",
+    "artist_location": "Fort Lauderdate, FL",
+    "artist_thumbnail": "http://cdn3.minted.com/files/content/designers/200_189222_53cacbb4e002c.jpg"
+  }
+];
