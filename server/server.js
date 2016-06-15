eb3f269f880491e0ac746a1856d1ba11fc1332b0
Reina Mei <reinameic@gmail.com>
2016-06-15 13:34:00 -0700

    Create basic server

--- server/server.js
+++ server/server.js
@@ -2,3 +2,11 @@ var express = require('express');
 var app = express();
 
 var db = require('./db.js');
+
+app.use(express.static(__dirname + '/../client'));
+
+app.listen(3000, function() {
+  console.log('artRater listening on port 3000...');
+});
+
+module.exports = app;
